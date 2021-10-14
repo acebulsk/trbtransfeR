c652f272ff906c1abd3ea9d7554348b97417bf61
Alex Cebulski <alexcebulski@gmail.com>
2021-10-14 10:48:28 -0600

    important fix to add density to calc

--- R/monin_obukhov_sim.R
+++ R/monin_obukhov_sim.R
@@ -9,21 +9,25 @@
 #'
 #' @param u_star friction velocity (m/s)
 #' @param T_c air temperature (celsuis)
+#' @param rho_air density of moist air
 #' @param z measurement height (m)
 #' @param Q_h surface kinematic heat flux (w / m2)
 #' @param d_0 displacement height (m)
 #' @param k von karman constant (0.4)
 #' @param g acceleration due to gravity (9.81 m s -2 )
+#' @param C_p 1004.67 (specific heat at constant pressure J kg-1 K-1
 #'
 #'
 #' @return L in metres. stability (x) = z - d_0 / L
 #' @export
 #'
 #' @examples
-monin_obukhov_sim <- function(u_star, T_c, z, Q_h, d_0, g = 9.81, k = 0.4){
+monin_obukhov_sim <- function(u_star, T_c, rho_air, z, Q_h, d_0, g = 9.81, k = 0.4, C_p = 1004.67){
     T_k <- T_c + 273.15
-    L <- (u_star^3 * T_k) / (k * g * Q_h)
-
+    L <- dplyr::if_else(Q_h == 0,
+                   1e6,
+                   (u_star^3 * T_k * rho_air * C_p) / (k * g * Q_h)
+                   )
     return(L)
 }
 
